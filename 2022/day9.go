c8b6e8b960193128f0acd93952f7bc838450f38a
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2022-12-27 11:34:17 +0700

    fix(update-pos): Couldn't figure out how to update each knot's position correctly yet

--- 2022/day9.go
+++ 2022/day9.go
@@ -38,32 +38,32 @@ func convertToMovements(input string) []Movement {
 	return movements
 }
 
-func getSign(num int) int {
-	if num > 0 {
+func getSign(num float64) float64 {
+	if num > 1 {
 		return 1
 	}
-	if num < 0 {
+	if num < -1 {
 		return -1
 	}
-	return 0
+	return num
 }
 
 // NOTE: `Knots := {Head(x, y), Tail(x, y)}`.
 
 type Knot struct {
-	x int
-	y int
+	x float64
+	y float64
 }
 
-func (k *Knot) New(newX, newY int) *Knot {
+func (k *Knot) New(newX, newY float64) *Knot {
 	return &Knot{
 		x: newX,
 		y: newY,
 	}
 }
 
-func (k *Knot) MoveWithDirection(m Movement) {
-	switch m.Direction {
+func (k *Knot) MoveWithDirection(direction string) {
+	switch direction {
 	case "R": // Right.
 		k.x += 1
 	case "L": // Left.
@@ -77,61 +77,65 @@ func (k *Knot) MoveWithDirection(m Movement) {
 	}
 }
 
-func (k Knot) DiffEachAxis(other Knot) (int, int) {
+func (k Knot) DiffEachAxis(other Knot) (float64, float64) {
 	return other.x - k.x, other.y - k.y
 }
 
 func (k *Knot) TailMoveToward(newHead Knot) {
 	tail := *k
 	disX, disY := newHead.DiffEachAxis(tail)
-	absX, absY := math.Abs(float64(disX)), math.Abs(float64(disY))
+	absX, absY := math.Abs(disX), math.Abs(disY)
 
 	// NOTE: If the `newHead` and the `oldTail` (k) have the maximum
-	// 			 distance (in absolute) between them are between the range of [-1; +1],
-	// 			 then we won't need to update anything.
+	// 	distance (in absolute) between them are between the range of [-1; +1],
+	// 	then we won't need to update anything.
 	if math.Max(absX, absY) <= 1 {
 		return
 	}
 
 	// NOTE: In each instruction of motion, the rope is moved by an atomic distance,
-	//			 or more specifically, by routing along each axis within the boundary of a digit.
-	if absY > absX {
-		k.x = newHead.x
-		k.y = newHead.y - getSign(disY)
-	} else if absY < absX {
-		k.y = newHead.y
-		k.x = newHead.x - getSign(disX)
-	} else {
-		// NOTE: `disX == disY`.
-		k.x = newHead.x - getSign(disX)
-		k.y = newHead.y - getSign(disY)
-	}
+	//	or more specifically, by routing along each axis within the boundary of an unit.
+	// if absY > absX {
+	// 	k.x = newHead.x
+	// 	k.y = newHead.y - getSign(disY)
+	// } else if absY < absX {
+	// 	k.y = newHead.y
+	// 	k.x = newHead.x - getSign(disX)
+	// } else {
+	// 	// NOTE: `disX == disY`.
+	// 	k.x = newHead.x - getSign(disX)
+	// 	k.y = newHead.y - getSign(disY)
+	// }
+	k.x += getSign(newHead.x)
+	k.y += getSign(newHead.y)
 }
 
 func solvingDay9Part1(data string) int {
 	motions := convertToMovements(data)
-	knots := [2]Knot{} // NOTE: `Rope := { (Tail, Head) := (Knot(x1,y1), Knot(x2, y2)) }`.
+	knots := [2]Knot{} // NOTE: `Rope := { (Head, Tail) := (Knot(x0, y0), Knot(x1, y1)) }`.
 	hashSet := make(map[Knot]struct{})
 
 	// Solution2:
 	// trace := []Knot{}
 	// trace = append(trace, knots[1]) // NOTE: Tracing after each tail's last position, and recorded by an array.
 
-	for _, m := range motions[:10] {
+	for _, m := range motions[:20] {
 		for step := 0; step < m.Steps; step++ {
 			for idx := range knots {
 				// NOTE: `head` move normally.
 				if idx == 0 {
-					knots[idx].MoveWithDirection(m)
+					knots[idx].MoveWithDirection(m.Direction)
 					continue
 				}
+
 				head := knots[idx-1]
 				knots[idx].TailMoveToward(head)
 			} // Move both knots.
 
-			hashSet[knots[len(knots)-2]] = struct{}{} // Adding each knot-tail's position into the hash set.
+			hashSet[knots[len(knots)-1]] = struct{}{} // Adding each knot-tail's position into the hash set.
+			fmt.Println(m)
 			fmt.Println(knots)
-			fmt.Println(hashSet)
+			// fmt.Println(hashSet)
 		} // Finish one motion from one instruction.
 	} // Finish all motions as the instruction list.
 
